b8bc2f4f142179aad59d4d6db9476e07e631a9a0
Kaio Henrique <kaiohenriquemalaquias@gmail.com>
2026-04-24 19:14:34 -0300

    New mode: Light

--- js/main.js
+++ js/main.js
@@ -32,3 +32,26 @@ revealElements.forEach(el => {
 el.classList.add("reveal");
 revealObserver.observe(el);
 });
+/* Tema */
+const themeBtn = document.getElementById("theme-toggle");
+function applyTheme(theme) {
+  document.body.classList.remove("dark", "light");
+  document.body.classList.add(theme);
+  localStorage.setItem("theme", theme);
+  updateThemeIcon(theme);
+}
+function updateThemeIcon(theme) {
+  if (!themeBtn) return;
+  themeBtn.innerHTML = `
+    <i data-lucide="${theme === "dark" ? "sun" : "moon"}"></i>
+  `;
+  lucide.createIcons();
+}
+const savedTheme = localStorage.getItem("theme") || "dark";
+applyTheme(savedTheme);
+if (themeBtn) {
+  themeBtn.addEventListener("click", () => {
+    const newTheme = document.body.classList.contains("dark") ? "light" : "dark";
+    applyTheme(newTheme);
+  });
+}
